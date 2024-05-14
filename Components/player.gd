8839111d6c410d96a5c4039453177164e383f6af
ProudOne7 <peternyen7@gmail.com>
2024-05-14 16:43:10 -0500

    Edited Movement Logic

--- Components/player.gd
+++ Components/player.gd
@@ -1,12 +1,13 @@
 extends Area2D
 
-@export var speed = 400 # How fast the player will move (pixels/sec).
-var screen_size # Size of the game window.
+@export var speed = 400 
+var screen_size 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,3 +26,8 @@ func _process(delta):
 	# Prevent offscreen movement
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
