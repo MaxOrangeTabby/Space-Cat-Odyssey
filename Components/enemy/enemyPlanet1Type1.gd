2b22fa6ea8a3a2702fcfec39352d944e1f9497ed
ProudOne7 <peternyen7@gmail.com>
2024-05-16 19:06:28 -0500

    Fixed Enemy Movement Logic

--- Components/enemy/enemyPlanet1Type1.gd
+++ Components/enemy/enemyPlanet1Type1.gd
@@ -1,16 +1,16 @@
 extends RigidBody2D
 
+var enemySpeed = -5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
-	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	global_position.x += enemySpeed 
 
 
+	
 func _on_visible_on_screen_enabler_2d_screen_exited():
 	queue_free()
