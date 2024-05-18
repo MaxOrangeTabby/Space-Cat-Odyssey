bcd3c17ddc9eb0efb47c4d7672542df72bda714a
ProudOne7 <peternyen7@gmail.com>
2024-05-18 13:39:40 -0500

    Added Attack Area Logic

--- Components/player/player.gd
+++ Components/player/player.gd
@@ -5,7 +5,7 @@ const gravity = 35
 
 @export var speed = 500
 
-var attacking = false	
+var attacking = false
 var screen_size 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,13 +16,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	velocity.x = 0
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("move_right") and !attacking:
 		velocity.x += speed
 		$AnimatedSprite2D.play()
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("move_left") and !attacking:
 		velocity.x -= speed
 		$AnimatedSprite2D.play()
-	if Input.is_action_pressed("jump"):
+	if Input.is_action_pressed("jump") and !attacking:
 		if is_on_floor():
 			velocity.y = -1250	
 	if not is_on_floor():
@@ -42,12 +42,19 @@ func _process(delta):
 	if Input.is_action_just_pressed("primary_attack"):
 		$AnimatedSprite2D.play("primary_attack")
 		attacking = true;
-		$AttackArea/CollisionShape2D.disabled = false;
+		
+		if($AnimatedSprite2D.flip_h):
+			$AttackArea/AttackAreaLeft.disabled = false;
+		if(!$AnimatedSprite2D.flip_h):
+			$AttackArea/AttackAreaRight.disabled = false;
 
 
 func _on_animated_sprite_2d_animation_finished():
 	if $AnimatedSprite2D.animation == "primary_attack":
-		$AttackArea/CollisionShape2D.disabled = true;
+		if($AnimatedSprite2D.flip_h):
+			$AttackArea/AttackAreaLeft.disabled = true;
+		if(!$AnimatedSprite2D.flip_h):
+			$AttackArea/AttackAreaRight.disabled = true;
 		attacking = false;
 
 func _on_body_entered(body):
