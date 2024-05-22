7da81618ff432a6ac01cf93bdad2649aa60c3c1d
ProudOne7 <peternyen7@gmail.com>
2024-05-22 15:24:45 -0500

    Updated Animation Logic

--- Components/player/player.gd
+++ Components/player/player.gd
@@ -9,6 +9,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var direction : Vector2 = Vector2.ZERO
 var animation_lock : bool = false
+var attacking : bool = false
 
 
 
@@ -23,10 +24,14 @@ func _physics_process(delta):
 
 	# Movement Logic (left/right)
 	direction = Input.get_vector("move_left","move_right","jump","move_down")
-	if direction.x != 0 && sprite_animation.animation != "jump":
+	if direction.x != 0 && sprite_animation.animation != "jump" && not animation_lock:
 		velocity.x = direction.x * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
+		
+	# Attack Logic
+	if Input.is_action_pressed("primary_attack"):
+			attack()
 
 	move_and_slide()
 	update_animation()
@@ -35,7 +40,21 @@ func _physics_process(delta):
 func _on_animated_sprite_2d_animation_finished():
 	if $AnimatedSprite2D.animation == "jump":
 		animation_lock = false
-
+	if $AnimatedSprite2D.animation == "primary_attack":
+		if($AnimatedSprite2D.flip_h):
+			$AttackArea/AttackAreaLeft.disabled = true
+		else:
+			$AttackArea/AttackAreaRight.disabled = true
+		animation_lock = false
+ 
+func attack():
+	if($AnimatedSprite2D.flip_h):
+		$AttackArea/AttackAreaLeft.disabled = false
+	else:
+		$AttackArea/AttackAreaRight.disabled = false
+	sprite_animation.play("primary_attack")
+	animation_lock = true
+	
 func jump():
 	velocity.y = jump_velocity
 	sprite_animation.play("jump")
@@ -43,8 +62,8 @@ func jump():
 
 func update_animation():
 	if not animation_lock:
-		if direction.x != 0:
-			sprite_animation.play("walk")
+		if direction.x != 0 && !attacking:
+			sprite_animation.play("run")
 			sprite_animation.flip_h = velocity.x < 0
 		else:
 			sprite_animation.play("idle")
