181934fc7a5521787cc97e9f55a9c22dc78c263a
ProudOne7 <peternyen7@gmail.com>
2024-05-24 13:43:12 -0500

    Added Knockback Func Call

--- Components/Player/Sword1.gd
+++ Components/Player/Sword1.gd
@@ -1,8 +1,13 @@
 extends Area2D
 
-@export var damage : float = 100
+@export var damage : int = 100
 
 func _on_body_entered(body):
 	for child in body.get_children():
 		if child is Damagable:
 			child.hit(damage)
+			# Knockback logic
+			if((get_parent().get_node("AnimatedSprite2D")).flip_h):
+				child.recieve_knockback(Vector2(1,0), damage)
+			else:
+				child.recieve_knockback(Vector2(-1,0), damage)
