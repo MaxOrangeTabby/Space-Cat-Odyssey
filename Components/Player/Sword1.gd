e630053211f8eb9f9ef3faf65f2a8f649215e261
ProudOne7 <peternyen7@gmail.com>
2024-05-28 23:56:23 -0500

    Give weapon knockback val

--- Components/Player/Sword1.gd
+++ Components/Player/Sword1.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @export var damage : int = 100
+@export var knockback : int = 40
 
 func _on_body_entered(body):
 	for child in body.get_children():
@@ -8,6 +9,6 @@ func _on_body_entered(body):
 			child.hit(damage)
 			# Knockback logic
 			if((get_parent().get_node("AnimatedSprite2D")).flip_h):
-				child.recieve_knockback(Vector2(1,0), damage)
+				child.recieve_knockback(Vector2(1,0), knockback)
 			else:
-				child.recieve_knockback(Vector2(-1,0), damage)
+				child.recieve_knockback(Vector2(-1,0), knockback)
